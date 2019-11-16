b051026395533b7e3be66be3ab85b0a50abba4e5
yoav nir <11yoav.nir@gmail.com>
2019-11-16 17:55:57 +0000

    SD card working, no utils

---
+++ include/sd_utils.h
@@ -0,0 +1,8 @@
+/*
+set of helper functions for the sd-card module
+*/
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
+char* sd_setup();
--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,85 @@
 
 #include <Arduino.h>
 #include <Ticker.h>
+#include <SD.h>
 
-#define END_STREAM 000000
+String headers = "0_ACC_X,0_ACC_Y,0_ACC_Z,0_GYR_X,0_GYR_Y,0_GYR_Z,1_ACC_X,1_ACC_Y,1_ACC_Z,1_GYR_X,1_GYR_Y,1_GYR_Z";
 bool status;
-int16_t incomingData[6];
+int16_t incomingData[7];
 int i = 0;
 bool complete_set;
 
+File myFile;
+int name_index = 0;
+char file_name [13];
+
 void setup() {
     Serial.begin(9600);
-    Wire.begin();
 
+    if (!SD.begin(4)) {
+        Serial.println("initialization failed!");
+        while (1);
+    }
+    myFile = SD.open("meta.txt");
+    if(myFile) {
+        while(myFile.available()) {
+            if(myFile.read() == 112){
+                name_index++;
+            }
+        }
+        myFile.close();
+    }
+    sprintf(file_name, "spr%d.csv", name_index);
+    myFile = SD.open("meta.txt", FILE_WRITE);
+    if(myFile) {
+        myFile.println(file_name);
+        myFile.close();
+    }
+
+    myFile = SD.open(file_name, FILE_WRITE);
+    if(myFile){
+        Serial.println("writing file ..");
+        myFile.println(headers);
+        myFile.close();
+    }
+
+    Wire.begin();
     mpu_setup();
     network_connect();
-
 }
-
 
 void loop() {
     if( NODE_ID == 00) {
         complete_set = false;
         if(network_avaiable()) {
             incomingData[i] = network_read();
-            i = (i+1) % 6;
+            i = (i+1) % 7;
             if(i == 0) {
                 complete_set = true;
             }
         }
         if(complete_set) {
-            Serial.println();
+            int16_t* p = mpu_get();
+            myFile = SD.open(file_name, FILE_WRITE);
             for (int i = 0; i < 6; i++) {
-                Serial.print(incomingData[i]);
-                Serial.print(",");
+                myFile.print(*(p+i));
+                myFile.print(",");
             }
+            for (int i = 1; i < 7; i++) {
+                myFile.print(incomingData[i]);
+                myFile.print(",");
+            }
+            myFile.println();
+            myFile.close();
         }
     } else {
         if(millis() % 100 == 0){
             int16_t* p = mpu_get();
-            for (int i = 0; i < 6; i++) {
-                status = network_write(*(p+i));
+            status = network_write(1);
+            if(status){
+                for (int i = 0; i < 6; i++) {
+                    status = network_write(*(p+i));
+                }
             }
         }
     }
