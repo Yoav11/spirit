c210172e6a5b9046dd36240ac9a5158203d41d25
yoav nir <11yoav.nir@gmail.com>
2020-01-23 18:03:01 +0000

    radio test

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,51 @@
-#include <mpu_utils.h>
-#include <nrf_utils.h>
-#include <led_utils.h>
+// 18 Mar 2018 - simple program to verify connection between Arduino
+//      and nRF24L01+
+//  This program does NOT attempt any communication with another nRF24
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 #include <Arduino.h>
-#include <Ticker.h>
-#include <SD.h>
 
-String headers = "0_ACC_X,0_ACC_Y,0_ACC_Z,0_GYR_X,0_GYR_Y,0_GYR_Z,1_ACC_X,1_ACC_Y,1_ACC_Z,1_GYR_X,1_GYR_Y,1_GYR_Z";
-bool status;
-int16_t incomingData[7];
-int i = 0;
-bool complete_set;
+#include <printf.h>
 
-File myFile;
-int name_index = 0;
-char file_name [13];
+#define CE_PIN   9
+#define CSN_PIN 10
 
-float blink_count = 0;
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+char dataReceived[10]; // this must match dataToSend in the TX
+bool newData = false;
+
 
 void setup() {
     Serial.begin(9600);
-    if( NODE_ID == 00) {
-        if (!SD.begin(4)) {
-            Serial.println("initialization failed!");
-            while (1);
-        }
-        myFile = SD.open("meta.txt");
-        if(myFile) {
-            while(myFile.available()) {
-                if(myFile.read() == 112){
-                    name_index++;
-                }
-            }
-            myFile.close();
-        }
-        sprintf(file_name, "spr%d.csv", name_index);
-        myFile = SD.open("meta.txt", FILE_WRITE);
-        if(myFile) {
-            myFile.println(file_name);
-            myFile.close();
-        }
+    printf_begin();
 
-        myFile = SD.open(file_name, FILE_WRITE);
-        if(myFile){
-            Serial.println("writing file ..");
-            myFile.println(headers);
-            myFile.close();
-        }
-    }
-
-    pinMode(BLUE_LED, OUTPUT);
-    Wire.begin();
-    mpu_setup();
-    network_connect();
+    Serial.println("CheckConnection Starting");
+    Serial.println();
+    Serial.println("FIRST WITH THE DEFAULT ADDRESSES after power on");
+    Serial.println("  Note that RF24 does NOT reset when Arduino resets - only when power is removed");
+    Serial.println("  If the numbers are mostly 0x00 or 0xff it means that the Arduino is not");
+    Serial.println("     communicating with the nRF24");
+    Serial.println();
+    radio.begin();
+    radio.printDetails();
+    Serial.println();
+    Serial.println();
+    Serial.println("AND NOW WITH ADDRESS AAAxR  0x41 41 41 78 52   ON P1");
+    Serial.println(" and 250KBPS data rate");
+    Serial.println();
+    radio.openReadingPipe(1, thisSlaveAddress);
+    radio.setDataRate( RF24_250KBPS );
+    radio.printDetails();
+    Serial.println();
+    Serial.println();
 }
 
+
 void loop() {
-    if( NODE_ID == 00) {
-        complete_set = false;
-        if(network_avaiable()) {
-            incomingData[i] = network_read();
-            i = (i+1) % 7;
-            if(i == 0) {
-                complete_set = true;
-            }
-        }
-        if(complete_set) {
-            int16_t* p = mpu_get();
-            myFile = SD.open(file_name, FILE_WRITE);
-            if(myFile) {
-                for (int i = 0; i < 6; i++) {
-                    myFile.print(*(p+i));
-                    myFile.print(",");
-                }
-                for (int i = 1; i < 7; i++) {
-                    myFile.print(incomingData[i]);
-                    myFile.print(",");
-                }
-                myFile.println();
-                myFile.close();
-                if(millis() - blink_count >= 1000) {
-                    blink_count = millis();
-                    blink_blue();
-                }
-            }
-        }
-    } else {
-        if(millis() % 100 == 0){
-            int16_t* p = mpu_get();
-            status = network_write(1);
-            if(status){
-                for (int i = 0; i < 6; i++) {
-                    status = network_write(*(p+i));
-                }
-                if(status && millis() - blink_count >= 1000) {
-                    blink_count = millis();
-                    blink_blue();
-                }
-            }
-        }
-    }
+
 }
--- src/utils/nrf_utils.cpp
+++ src/utils/nrf_utils.cpp
@@ -1,30 +1,30 @@
 #include <nrf_utils.h>
 
-RF24 radio(NRF_CE, NRF_CSN);
-RF24Network network(radio);
-
-void network_connect() {
-    SPI.begin();
-    radio.begin();
-    network.begin(90, NODE_ID);
-}
-
-int16_t network_read() {
-    int16_t incomingData;
-    network.update();
-    RF24NetworkHeader header;
-    network.read(header, &incomingData, sizeof(incomingData));
-    return(incomingData);
-}
-
-bool network_write(int16_t data) {
-    network.update();
-    RF24NetworkHeader header(00);
-    bool ok = network.write(header, &data, sizeof(data));
-    return(ok);
-}
-
-bool network_avaiable() {
-    network.update();
-    return(network.available());
-}
+// RF24 radio(NRF_CE, NRF_CSN);
+// RF24Network network(radio);
+//
+// void network_connect() {
+//     SPI.begin();
+//     radio.begin();
+//     network.begin(90, NODE_ID);
+// }
+//
+// int16_t network_read() {
+//     int16_t incomingData;
+//     network.update();
+//     RF24NetworkHeader header;
+//     network.read(header, &incomingData, sizeof(incomingData));
+//     return(incomingData);
+// }
+//
+// bool network_write(int16_t data) {
+//     network.update();
+//     RF24NetworkHeader header(00);
+//     bool ok = network.write(header, &data, sizeof(data));
+//     return(ok);
+// }
+//
+// bool network_avaiable() {
+//     network.update();
+//     return(network.available());
+// }
